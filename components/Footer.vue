ec17ca5aa4ca5df20607b4a170731f8cc36665c8
DeathHackz <xdeathhackzx@gmail.com>
2020-05-07 19:01:55 -0400

    removed class

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-dark py-3 fixed-bottom">
+  <footer class="bg-dark py-3">
     <b-container>
       <p class="text-white text-center my-0">
         Copyright &copy;DeathHackz 2018 - {{ year }}
