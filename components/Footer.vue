ab92260378ad3bc6fe013fdee5bbe738abd63738
DeathHackz <xdeathhackzx@gmail.com>
2020-05-07 17:22:43 -0400

    created footer

---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="bg-dark py-3 fixed-bottom">
+    <b-container>
+      <p class="text-white text-center my-0">
+        Copyright &copy;DeathHackz 2018 - {{ year }}
+      </p>
+    </b-container>
+  </footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    year: new Date().getFullYear()
+  })
+}
+</script>
